b8dc23d6b3b36b4fe49ab071e03638227b1e76b5
Mikkel Ricky <mikkel@mikkelricky.dk>
2024-10-14 14:43:29 +0200

    Release v1.3.0

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 
 const appName = "markdown-code-runner"
 const defaultFilename = "README.md"
-const version = "v1.2.1"
+const version = "v1.3.0"
 const ARG_FILENAME = "file"
 const FILENAME_STDIN = "-"
 const ARG_SUBSTITUTIONS = "substitutions"
